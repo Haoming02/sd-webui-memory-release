88266ff6e536a04ed3ccd6ac7c075810cbc3ccb2
Haoming <hmstudy02@gmail.com>
2023-07-17 19:52:55 +0800

    New... feature?
    Oh look. An update!?

--- README.md
+++ README.md
@@ -2,19 +2,23 @@
 This is an Extension for the [Automatic1111 Webui](https://github.com/AUTOMATIC1111/stable-diffusion-webui), that calls `torch.cuda.empty_cache()` after each generation.
 
 ### But Why?
-- A few Reddit posts/comments mention that CUDA sometimes has memory issues, 
-and if you trigger the **Out of Memory Error**, it can continue to leak memory still.
-This Extension *tried to* solve that, by calling `torch.cuda.empty_cache()` after each generation.
+- A few Reddit posts/comments mention that CUDA sometimes has memory issues, and the webui may cause memory leaks. 
+This Extension *tried to* mediate that by calling `torch.cuda.empty_cache()` after each generation.
 
-- However, turns out `postprocess` is not called when the generation fails.
-Therefore, I added a button to manually call `torch.cuda.empty_cache()`.
+### "Features"
+- Turns out `postprocess` is not called when a generation fails. Thus, I added a button to manually try releasing the memory.
 
-- Toggle `Debug` to see if the function is actually being called.
+- Also comes with another button that unloads and reloads the checkpoint for the maximum cleanse.
+    - ~~*This just calls the same functions that you can already find in the **Actions** section of the **Settings** tab*~~
+
+- In the **System** section of the **Settings** tab, you can also turn on debug log or enable additionally refreshing the `torch` networks.
 
 <p align="center"><img src="Sample.jpg"></p>
 
 <hr>
 
-<sup>[1] I'm 99.9% sure that this Extension helps nothing. You most likely do ***not*** need this. </sup>
+<sup>[1] I'm 99.99% sure that this Extension helps nothing. You most likely do **not** need this. </sup>
 
 <sup>[2] Shout out to [`@kgmkm_mkgm`](https://twitter.com/kgmkm_mkgm/status/1658760768958140418) for sharing this Extension with tens of thousands of people. </sup>
+
+<sup>[3] ~~Seriously, why are people downloading this? Why are people even Star-ing this? What does this actually help?~~ </sup>
--- scripts/release.py
+++ scripts/release.py
@@ -1,22 +1,59 @@
-﻿import modules.scripts as scripts
-import modules.devices as devices
-import gc
+﻿from modules import script_callbacks, shared
+import modules.sd_models as models
+import modules.scripts as scripts
 import gradio as gr
+import torch
+import gc
 
-def mem_release(debug = True):
+cleanNN = None
+
+def refresh_LoRA():
+    global cleanNN
+    torch.nn.Linear.forward = cleanNN[0]
+    torch.nn.Linear._load_from_state_dict = cleanNN[1]
+    torch.nn.Conv2d.forward = cleanNN[2]
+    torch.nn.Conv2d._load_from_state_dict = cleanNN[3]
+    torch.nn.MultiheadAttention.forward = cleanNN[4]
+    torch.nn.MultiheadAttention._load_from_state_dict = cleanNN[5]
+
+def init_nn():
+    global cleanNN
+    cleanNN = [
+        torch.nn.Linear.forward,
+        torch.nn.Linear._load_from_state_dict,
+        torch.nn.Conv2d.forward,
+        torch.nn.Conv2d._load_from_state_dict,
+        torch.nn.MultiheadAttention.forward,
+        torch.nn.MultiheadAttention._load_from_state_dict
+    ]
+
+def mem_release():
     try:
         gc.collect()
-        devices.torch_gc
+        torch.cuda.empty_cache()
+        torch.cuda.ipc_collect()
         gc.collect()
+    
+        if shared.opts.memre_dolora and shared.opts.memre_dolora is True:
+            refresh_LoRA()
 
-        if debug == True:
-            print('\n Memory Released!\n')
+        if shared.opts.memre_debug and shared.opts.memre_debug is True:
+            print('\nMemory Released!\n')
 
     except:
-        if debug == True:
-            print('\n\n Memory Release Failed...!\n\n')
+        if shared.opts.memre_debug and shared.opts.memre_debug is True:
+            print('\n[Warning] Memory Release Failed...!\n')
+
+def reload_models():
+    models.unload_model_weights()
+    mem_release()
+    models.reload_model_weights()
 
 class Script(scripts.Script):
+    def __init__(self):
+        global cleanNN
+        if cleanNN is None:
+            init_nn()
 
     def title(self):
         return "Memory Release"
@@ -27,13 +64,19 @@ class Script(scripts.Script):
     def ui(self, is_img2img):
         with gr.Accordion('Memory Release', open=False):
             with gr.Row():
-                reload_button = gr.Button('🧹')
-                reload_button.click(fn=mem_release)
+                release_button = gr.Button('🧹')
+                reload_button = gr.Button('💥')
 
-                debug = gr.Checkbox(label='Debug')
+            release_button.click(fn=mem_release)
+            reload_button.click(fn=reload_models)
 
-        return [debug]
-
-    def postprocess(self, p, processed, debug:bool):
-        mem_release(debug)
         return None
+
+    def postprocess(self, *args):
+        mem_release()
+
+def on_ui_settings():
+    shared.opts.add_option("memre_debug", shared.OptionInfo(False, "Memory Release - Debug", section=("system", "System")))
+    shared.opts.add_option("memre_dolora", shared.OptionInfo(False, "Memory Release - Refresh LoRA", section=("system", "System")))
+
+script_callbacks.on_ui_settings(on_ui_settings)

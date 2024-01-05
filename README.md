bb2e893b7edd5b96a25bd9cf58e95cd9d2e3652f
Haoming <hmstudy02@gmail.com>
2024-01-05 12:12:12 +0800

    Cleanups
    Removed outdated junks...

--- README.md
+++ README.md
@@ -2,23 +2,27 @@
 This is an Extension for the [Automatic1111 Webui](https://github.com/AUTOMATIC1111/stable-diffusion-webui), that calls `torch.cuda.empty_cache()` after each generation.
 
 ### But Why?
-- A few Reddit posts/comments mention that CUDA sometimes has memory issues, and the webui may cause memory leaks. 
-This Extension *tried to* mediate that by calling `torch.cuda.empty_cache()` after each generation.
+- A few Reddit posts/comments mentioned that CUDA sometimes can cause memory issues/leaks. 
+This Extension *tries to* mediate that by calling `torch.cuda.empty_cache()` after each generation.
 
 ### "Features"
-- Turns out `postprocess` is not called when a generation fails. Thus, I added a button to manually try releasing the memory.
+- `gc.collect()` and `torch.cuda.empty_cache()` after every generation
+- A *placebo*™️ button to manually free the memory
+- Another button to reload the checkpoint
+    - *This is just the same built-in functions in the **Actions** section of the **Settings** tab*
 
-- Also comes with another button that unloads and reloads the checkpoint for the maximum cleanse.
-    - ~~*This just calls the same functions that you can already find in the **Actions** section of the **Settings** tab*~~
-
-- In the **System** section of the **Settings** tab, you can also turn on debug log or enable additionally refreshing the `torch` networks.
-
-<p align="center"><img src="Sample.jpg"></p>
+<p align="center">
+<img src="Sample.jpg" width=512>
+</p>
 
 <hr>
 
-<sup>[1] I'm 99.99% sure that this Extension helps nothing. You most likely do **not** need this. </sup>
+<sup>[0] Webui nowdays does a pretty good job cleaning the memory. The graph above is severely outdated. </sup>
+
+<sup>[1] I'm 99.999% sure that this Extension helps nothing. You most likely do **not** need this. </sup>
 
 <sup>[2] Shout out to [`@kgmkm_mkgm`](https://twitter.com/kgmkm_mkgm/status/1658760768958140418) for sharing this Extension with tens of thousands of people. </sup>
 
-<sup>[3] ~~Seriously, why are people downloading this? Why are people even Star-ing this? What does this actually help?~~ </sup>
+<sup>[3] ~~Seriously, why are people downloading this? Why do people even Star this? What does this actually help?~~ </sup>
+
+<sup>[4] ~~Why do I still update this? Eh, it doesn't hurt to install anyway.~~ </sup>
--- scripts/release.py
+++ scripts/release.py
@@ -5,55 +5,27 @@ import gradio as gr
 import torch
 import gc
 
-cleanNN = None
-
-def refresh_LoRA():
-    global cleanNN
-    torch.nn.Linear.forward = cleanNN[0]
-    torch.nn.Linear._load_from_state_dict = cleanNN[1]
-    torch.nn.Conv2d.forward = cleanNN[2]
-    torch.nn.Conv2d._load_from_state_dict = cleanNN[3]
-    torch.nn.MultiheadAttention.forward = cleanNN[4]
-    torch.nn.MultiheadAttention._load_from_state_dict = cleanNN[5]
-
-def init_nn():
-    global cleanNN
-    cleanNN = [
-        torch.nn.Linear.forward,
-        torch.nn.Linear._load_from_state_dict,
-        torch.nn.Conv2d.forward,
-        torch.nn.Conv2d._load_from_state_dict,
-        torch.nn.MultiheadAttention.forward,
-        torch.nn.MultiheadAttention._load_from_state_dict
-    ]
-
 def mem_release():
     try:
         gc.collect()
         torch.cuda.empty_cache()
         torch.cuda.ipc_collect()
         gc.collect()
-    
-        if shared.opts.memre_dolora and shared.opts.memre_dolora is True:
-            refresh_LoRA()
 
-        if shared.opts.memre_debug and shared.opts.memre_debug is True:
+        if getattr(shared.opts, 'memre_debug', False):
             print('\nMemory Released!\n')
 
     except:
-        if shared.opts.memre_debug and shared.opts.memre_debug is True:
-            print('\n[Warning] Memory Release Failed...!\n')
+        if getattr(shared.opts, 'memre_debug', False):
+            import traceback
+            traceback.print_exc()
 
 def reload_models():
     models.unload_model_weights()
     mem_release()
-    models.reload_model_weights()
+    models.send_model_to_device(shared.sd_model)
 
 class Script(scripts.Script):
-    def __init__(self):
-        global cleanNN
-        if cleanNN is None:
-            init_nn()
 
     def title(self):
         return "Memory Release"
@@ -63,6 +35,7 @@ class Script(scripts.Script):
 
     def ui(self, is_img2img):
         with gr.Accordion('Memory Release', open=False):
+            gr.Markdown('<h4 style="float: left;">Garbage Collect</h4> <h4 style="float: right;">Flush Checkpoint Memory</h4>')
             with gr.Row():
                 release_button = gr.Button('🧹')
                 reload_button = gr.Button('💥')
@@ -70,13 +43,17 @@ class Script(scripts.Script):
             release_button.click(fn=mem_release)
             reload_button.click(fn=reload_models)
 
-        return None
+        pass
+
+    def postprocess_batch(self, *args, **kwargs):
+        mem_release()
+        pass
 
     def postprocess(self, *args):
         mem_release()
+        pass
 
 def on_ui_settings():
     shared.opts.add_option("memre_debug", shared.OptionInfo(False, "Memory Release - Debug", section=("system", "System")))
-    shared.opts.add_option("memre_dolora", shared.OptionInfo(False, "Memory Release - Refresh LoRA", section=("system", "System")))
 
 script_callbacks.on_ui_settings(on_ui_settings)
